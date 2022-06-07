5cce79e039a4b9d568e16835ba9874843c6c803e
kevin0120 <jingwei1224@163.com>
2022-06-08 00:18:33 +0800

    docker

---
+++ docker/chartjs/customize/build_docker.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+version="0.0.6"
+
+docker_repo="ghcr.io/kevin0120/node-red/chartjs-origin"
+
+docker build -t ${docker_repo}:${version} .
+#docker build -t ${docker_repo}:latest .
+
+docker login ghcr.io -u kevin0120 -p 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
+docker push ${docker_repo}:${version}
+#docker push ${docker_repo}:latest
